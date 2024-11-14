02412555d4264fe518a5e4944d9ed84196c1c2fa
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-11-13 23:41:14 -0600

    Reeorganizacion de los puntos a tamaño B2.1

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credenciales.json'
 
 ADJUSTMENTS = {
-    1: {'x': -1, 'y': 4},  # Extremo derecho ceja izquierda
+    1: {'x': -3, 'y': 4},  # Extremo derecho ceja izquierda
     2: {'x': -2, 'y': 2},   # Extremo izquierdo ceja izquierda
-    3: {'x': 5, 'y': 0},  # Extremo derecho ceja derecha
-    4: {'x': -22, 'y': -2}, # Extremo izquierdo ceja derecha
+    3: {'x': 4, 'y': 0},  # Extremo derecho ceja derecha
+    4: {'x': -26, 'y': -2}, # Extremo izquierdo ceja derecha
     5: {'x': 2, 'y': 1},  # Centro del ojo izquierdo
     6: {'x': -5, 'y': 0},  # Lado izquierdo ojo izquierdo
-    7: {'x': 4, 'y': 3},   # Lado derecho ojo izquierdo
+    7: {'x': 4, 'y': 4},   # Lado derecho ojo izquierdo
     8: {'x': -2, 'y': 3},   # Centro del ojo derecho
     9: {'x': -5, 'y': 0},  # Lado izquierdo ojo derecho
     10: {'x': 1, 'y': 2},  # Lado derecho ojo derecho
     11: {'x': -1, 'y': 2},  # Punta de la nariz
     12: {'x': -2, 'y': 2},  # Labio superior izquierdo
-    13: {'x': -3, 'y': 1},  # Labio superior derecho
+    13: {'x': -2, 'y': 1},  # Labio superior derecho
     14: {'x': -4, 'y': 1},  # Labio inferior izquierdo
-    15: {'x': -3, 'y': 1}   # Labio inferior derecho
+    15: {'x': -1, 'y': 1}   # Labio inferior derecho
 }
 
 def detect_face_landmarks(image_path):
