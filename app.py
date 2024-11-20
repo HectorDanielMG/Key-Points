c8257a82bc085d264af3c45b9df27b4ad4e88661
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-11-20 09:17:19 -0600

    Reorganizacion de las dimenciones para tamaño C1.3.1

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ ADJUSTMENTS = {
     1: {'x': -2, 'y': 2},  # Extremo derecho ceja izquierda
     2: {'x': -2, 'y': 2},  # Extremo izquierdo ceja izquierda
     3: {'x': 4, 'y': 0},   # Extremo derecho ceja derecha
-    4: {'x': -23, 'y': -3},# Extremo izquierdo ceja derecha
+    4: {'x': -23, 'y': -2},# Extremo izquierdo ceja derecha
     5: {'x': 2, 'y': 1},   # Centro del ojo izquierdo
     6: {'x': -7, 'y': 0},  # Lado izquierdo ojo izquierdo
     7: {'x': 6, 'y': 1},   # Lado derecho ojo izquierdo
@@ -25,8 +25,8 @@ ADJUSTMENTS = {
     11: {'x': -1, 'y': 2}, # Punta de la nariz
     12: {'x': -2, 'y': 2}, # Labio superior izquierdo
     13: {'x': -3, 'y': 1}, # Labio superior derecho
-    14: {'x': -2, 'y': 1}, # Labio inferior izquierdo
-    15: {'x': -1, 'y': 1}  # Labio inferior derecho
+    14: {'x': -2, 'y': 2}, # Labio inferior izquierdo
+    15: {'x': -2, 'y': 1}  # Labio inferior derecho
 }
 
 def detect_face_landmarks(image_path):
