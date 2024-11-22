b808356236e181cf21329b94436edc33ea3e25cb
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-11-22 16:23:54 -0600

    Reorganizacion de las dimenciones para tamaño A2.1.0

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credenciales.json'
 
 # Ajustes de puntos faciales
 ADJUSTMENTS = {
-    1: {'x': -2, 'y': 2},  # Extremo derecho ceja izquierda
+    1: {'x': -2, 'y': 4},  # Extremo derecho ceja izquierda
     2: {'x': -2, 'y': 2},  # Extremo izquierdo ceja izquierda
     3: {'x': 4, 'y': 0},   # Extremo derecho ceja derecha
     4: {'x': -26, 'y': -2},# Extremo izquierdo ceja derecha
     5: {'x': 2, 'y': 1},   # Centro del ojo izquierdo
     6: {'x': -7, 'y': 0},  # Lado izquierdo ojo izquierdo
-    7: {'x': 6, 'y': 1},   # Lado derecho ojo izquierdo
+    7: {'x': 5, 'y': 1},   # Lado derecho ojo izquierdo
     8: {'x': -4, 'y': 1},  # Centro del ojo derecho
     9: {'x': -6, 'y': 1},  # Lado izquierdo ojo derecho
-    10: {'x': 4, 'y': 0},  # Lado derecho ojo derecho
+    10: {'x': 3, 'y': 0},  # Lado derecho ojo derecho
     11: {'x': -1, 'y': 2}, # Punta de la nariz
-    12: {'x': -2, 'y': 2}, # Labio superior izquierdo
-    13: {'x': -1, 'y': 1}, # Labio superior derecho
-    14: {'x': -1, 'y': 2}, # Labio inferior izquierdo
+    12: {'x': -3, 'y': 2}, # Labio superior izquierdo
+    13: {'x': -2, 'y': 1}, # Labio superior derecho
+    14: {'x': -2, 'y': 3}, # Labio inferior izquierdo
     15: {'x': -1, 'y': 1}  # Labio inferior derecho
 }
 
