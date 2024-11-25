652581bc9733d76a12283d894c0f86c2f0d8e921
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-11-24 22:10:54 -0600

    Reorganizacion de las dimenciones para tamaño A2.1.2

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ ADJUSTMENTS = {
     9: {'x': -5, 'y': 1},  # Lado izquierdo ojo derecho
     10: {'x': 3, 'y': 0},  # Lado derecho ojo derecho
     11: {'x': -0, 'y': 2}, # Punta de la nariz
-    12: {'x': -3, 'y': 2}, # Labio superior izquierdo
-    13: {'x': -1, 'y': 2}, # Labio superior derecho
-    14: {'x': -1, 'y': 1}, # Labio inferior izquierdo
-    15: {'x': -1, 'y': 1}  # Labio inferior derecho
+    12: {'x': -2, 'y': 2}, # Labio superior izquierdo
+    13: {'x': -2, 'y': 2}, # Labio superior derecho
+    14: {'x': -3, 'y': 1}, # Labio inferior izquierdo
+    15: {'x': -0, 'y': 1}  # Labio inferior derecho
 }
 
 def detect_face_landmarks(image_path):
