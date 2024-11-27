023da8e881dbc355bbf8470980bf4c5055072387
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-11-26 21:53:46 -0600

    Reorganizacion de las dimenciones para tamaño A3.1.1

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credenciales.json'
 
 # Ajustes de puntos faciales
 ADJUSTMENTS = {
-    1: {'x': -1, 'y': 3},  # Extremo derecho ceja izquierda
-    2: {'x': -2, 'y': 2},  # Extremo izquierdo ceja izquierda
+    1: {'x': -2, 'y': 3},  # Extremo derecho ceja izquierda
+    2: {'x': 2, 'y': 2},  # Extremo izquierdo ceja izquierda
     3: {'x': 1, 'y': 0},   # Extremo derecho ceja derecha
     4: {'x': -25, 'y': -2},# Extremo izquierdo ceja derecha
     5: {'x': 1, 'y': -1},   # Centro del ojo izquierdo
-    6: {'x': 0, 'y': 0},  # Lado izquierdo ojo izquierdo
-    7: {'x': 2, 'y': 3},   # Lado derecho ojo izquierdo
+    6: {'x': 1, 'y': 0},  # Lado izquierdo ojo izquierdo
+    7: {'x': 1, 'y': 3},   # Lado derecho ojo izquierdo
     8: {'x': -4, 'y': 1},  # Centro del ojo derecho
     9: {'x': -5, 'y': 1},  # Lado izquierdo ojo derecho
     10: {'x': 3, 'y': 0},  # Lado derecho ojo derecho
